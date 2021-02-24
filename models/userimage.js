7508afded48f7864d24dec713f877b0f82d37d78
Satyam B <sbharadw@kent.edu>
2021-02-23 23:12:29 -0500

    adding itemimage.js file and userimage.js file to the models folder

---
+++ models/userimage.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Userimage = sequelize.define('Userimage', {
+
+      title: {
+        type: DataTypes.STRING,
+        validate:{
+          len: [2],
+          notEmpty: true
+        }
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        validate:{
+          len: [2],
+          notEmpty: true
+        }
+      },
+
+      item_type: {
+        type: DataTypes.STRING,
+        validate:{
+          len: [2],
+          notEmpty: true
+        }
+      }
+
+    });
+
+    Item.associate = (models) => {
+        
+        Item.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false,
+          },
+        });
+      };
+
+
+    Item.associate = (models) => {
+        
+        Item.belongsTo(models.ItemCategory, {
+          foreignKey: {
+            allowNull: false,
+          },
+        });
+      };
+    
+    return Userimage;
+  
+};
