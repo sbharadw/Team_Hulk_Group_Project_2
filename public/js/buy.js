b17171d946bb247516f8add3d833a35aafd2a665
Natalia <nasozontova@gmail.com>
2021-03-04 01:32:13 -0500

    hello

--- public/js/buy.js
+++ public/js/buy.js
@@ -1,19 +1,11 @@
+
 const buyBtn = $("#buyBtn");
 console.log(buyBtn);
-
-
 buyBtn.on("click", event =>{
   event.preventDefault();
   console.log("working click buy button");
   window.location.replace("/buy");
-
 });
-
-   
-
-
-  
-  
 
 
   
